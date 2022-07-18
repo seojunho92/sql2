8a24ac027f1094d6b00cd126d37d0b491fb94237
a <a@a.com>
2022-07-18 18:31:22 +0900

    'first'

--- 05.sql
+++ 05.sql
@@ -93,4 +93,55 @@ group by department_id; -- where절 사용시 group by보다 먼저 사용해야한다.
 select department_id, max(salary)
 from employees
 where max(salary) > 50
-group by department_id; -- error                
+group by department_id; -- error   
+
+select job_id, sum(salary) payroll
+from employees
+where job_id not like '%REP%'
+group by job_id
+having sum(salary) > 13000
+order by payroll;
+
+-- 과제] 매니저ID, 매니저별 관리 직원들 중 최소월급을 조회하라.
+--      최소월급이 $6,000 초과여야 한다.
+select manager_id, min(salary)
+from employees
+where manager_id is not null
+group by manager_id
+having min(salary) > 6000
+order by 2 desc;
+---------------------------
+
+select max(avg(salary))
+from employees
+group by department_id;
+
+select sum(max(avg(salary)))
+from employees
+group by department_id; -- error, to deeply (너무 깊다) 그룹함수는 2개까지 중첩가능하다.
+
+select department_id, round(avg(salary))
+from employees
+group by department_id;
+
+select department_id, round(avg(salary))
+from employees; -- error 싱글과 그룹을 중첩시킬때는 group by가 필요하다.
+
+--과제] 2001년, 2002년, 2003년도별 입사자 수를 찾는다.
+select sum(decode(to_char(hire_date, 'yyyy'), '2001', 1, 0)) "2001년",
+    sum(decode(to_char(hire_date, 'yyyy'), '2002', 1, 0)) "2002년",
+    sum(decode(to_char(hire_date, 'yyyy'), '2003', 1, 0)) "2003년"
+from employees;
+
+select count(case when hire_date like '2001%' then 1 else null end) "2001년",
+    count(case when hire_date like '2002%' then 1 else null end) "2002년",
+    count(case when hire_date like '2003%' then 1 else null end) "2003년"
+from employees;   
+
+-- 과제] 직업별, 부서별 월급합을 조회하라.
+--      부서는 20, 50, 80 이다.
+select job_id, sum(decode(department_id, 20, salary)) "20번부서", 
+            sum(decode(department_id, 50, salary)) "50번부서",
+            sum(decode(department_id, 80, salary)) "80번부서"
+from employees
+group by job_id;
